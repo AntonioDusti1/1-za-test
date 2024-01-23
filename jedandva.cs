aa4bfbc5110a9c9227d411eca9307a1877192615
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-23 12:24:50 +0100

    Add files via upload

---
+++ jedandva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cijeli_decimalni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi cijeli i decimalni broj:");
+            string input = Console.ReadLine();
+
+            string[] broj = input.Split();
+            int cijeli = int.Parse(broj[0]);
+            double decimalni = double.Parse(broj[1]);
+
+            Console.WriteLine("Cijeli: " + cijeli);
+            Console.WriteLine("Decimalni broj: " + decimalni);
+            Console.ReadLine();
+        }
+    }
+}
